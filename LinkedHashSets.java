47cb3ae2e0e2a942c8f744e309f44b41a9fe3b7c
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-13 17:57:37 +0530

    Update LinkedHashSets.java

--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,23 @@
-public class LinkedHashSets{
-public static void main(String[]args){
-  // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+ package collection;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+//public class LinkedHashSets{
+   // public static void main(String[]args){
+      // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+    
+    
+public class LinkedHashset {
+    public static void main(String[] args) {
+        LinkedHashSet<Person> p=new LinkedHashSet<>();
+        p.add(new Person("Ammu", 18));
+        p.add(new Person("Queen", 100));
+        p.add(new Person("Bharath", 22));
+        Iterator<Person> Iterator = p.iterator();
+        while(Iterator.hasNext()){
+            Person person = Iterator.next();
+            System.out.println(p);
+        }
+         
+    }
 }
