c3c3190b99284f3263d508a2c6d95e0087f51295
Bharathmanne18 <bharathmanne19@gmail.com>
2024-08-13 17:41:31 +0530

    Update HashSets.java

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,37 @@
-public class HashSets{
-// Prepare Hashset object here and add person class objects in them iterate it by using for each
+ package collection;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name,int age){
+        this.name=name;
+        this.age=age;
+
+    }
+    public String getname(){
+     return name;
+    }
+    public int getage(){
+        return age;
+    }
+ 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && name.equals(person.name);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
 }
+     
